8d2e04f3e6b3909405a2c32269b138f1596029ef
Uriel Fuentes <urielfuentesc@gmail.com>
2021-02-10 18:42:06 -0600

    Initial Bootstrap addition.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import { useState } from "react";
-import Login from './components/Login'
-import Register from './components/Register'
-import Stats from './components/Stats'
-import MainSection from './components/MainSection'
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Stats from "./components/Stats";
+import MainSection from "./components/MainSection";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
-  const [userState, setUserState] = useState('login')
-
+  const [userState, setUserState] = useState("login");
 
   var rightPanel;
 
-  switch(userState) {
-    case 'login': 
-      rightPanel = <Login />
+  switch (userState) {
+    case "login":
+      rightPanel = <Login />;
       break;
-    case 'register':
-      rightPanel = <Register />
+    case "register":
+      rightPanel = <Register />;
       break;
-    case 'stats':
-      rightPanel = <Stats />
+    case "stats":
+      rightPanel = <Stats />;
       break;
     default:
-      rightPanel = <Login />
+      rightPanel = <Login />;
   }
 
   return (
-    <div id="app">
-      <MainSection />
-      {rightPanel}
-    </div>
+    <Container fluid>
+      <Row>
+      <Col xs={4}></Col>
+        <Col xs={4} className="justify-content-center">
+          <MainSection />
+        </Col>
+        <Col>{rightPanel}</Col>
+      </Row>
+    </Container>
   );
 }
 
